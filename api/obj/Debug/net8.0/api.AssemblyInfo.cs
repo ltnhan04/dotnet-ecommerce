d773508b5aa420677f50e755264f495a25905220
Nhan Luong <tunhan281204@gmail.com>
2025-06-03 10:57:55 +0700

    chore: remove MongoDB and Redis connection classes

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1e961bae2411f926693d2dc608a0892480367ba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28dcc5da211199321989883129eb5ceb744f31f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
